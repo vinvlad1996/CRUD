e92d1de2805eb0ec2d2a9e9bd333291405263e10
Владимир Виноградов <vladimirvinogradov@MacBook-Air-Vladimir.local>
2024-03-30 01:40:17 +0300

    Add store

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,66 +1,47 @@
 <template>
-  <!-- Отображение индикатора загрузки, пока данные загружаются -->
   <a-spin v-if="loadingPosts" />
   <div v-if="!loadingPosts">
-    <!-- Заголовок страницы -->
     <h1>Posts</h1>
-    <!-- Кнопка для открытия модального окна добавления нового поста -->
     <a-button class="cards-create" @click="showAddPostModal"><PlusOutlined />Create new post</a-button>
-    <!-- Контейнер для отображения списка постов -->
     <div class="cards">
-      <!-- Итерация по массиву постов и отображение каждого поста -->
       <div class="cards-item" v-for="post in visiblePosts" :key="post.id">
-        <!-- Заголовок и кнопки действий для каждого поста -->
         <div class="cards-item-title">
           <h3>{{ post.title }}</h3>
-          <!-- Кнопки редактирования и удаления поста -->
           <div class="cards-item-buttons">
             <a-button @click="editPost(post)"><EditOutlined /></a-button>
             <a-button @click="deletePost(post.id)"><DeleteOutlined /></a-button>
           </div>
         </div>
-        <!-- Текст поста -->
         <p>{{ post.body }}</p>
       </div>
     </div>
-    <!-- Кнопка для открытия модального окна добавления нового поста (дублирующая) -->
     <div class="cards-item-buttons">
       <a-button class="cards-create" @click="showAddPostModal"><PlusOutlined />Create new post</a-button>
       <a-button v-if="shouldShowLoadMoreButton()" @click="loadMorePosts">Load More</a-button>
     </div>
-    <!-- Модальное окно для добавления нового поста -->
     <a-modal :footer="null" v-model:open="addPostModalVisible">
       <template #default>
-        <!-- Форма для добавления нового поста -->
         <form class="cards-form" @submit.prevent="addOrSavePost">
-          <!-- Поле для ввода заголовка -->
           <a-form-item label="Title" :required="true">
             <a-input :value="newPostTitle" @update:value="newPostTitle = $event" />
           </a-form-item>
-          <!-- Поле для ввода текста поста -->
           <a-form-item label="Body" :required="true">
             <a-textarea :value="newPostBody" @update:value="newPostBody = $event" />
           </a-form-item>
-          <!-- Кнопка для сохранения нового поста -->
           <a-button type="primary" html-type="submit">Save</a-button>
         </form>
       </template>
     </a-modal>
 
-    <!-- Модальное окно для редактирования существующего поста -->
     <a-modal :footer="null" v-model:open="editModalVisible">
       <template #default>
-        <!-- Форма для редактирования существующего поста -->
         <form class="cards-form" @submit.prevent="saveEditedPost">
-          <!-- Поле для редактирования заголовка -->
           <a-form-item label="Title" :required="true">
             <a-input :value="formData.title" @update:value="formData.title = $event" id="editTitle" />
           </a-form-item>
-          <!-- Поле для редактирования текста поста -->
           <a-form-item label="Body" :required="true">
             <a-textarea :value="formData.body" @update:value="formData.body = $event" id="editBody" />
           </a-form-item>
-          <!-- Кнопка для сохранения отредактированного поста -->
           <button type="submit">Save</button>
         </form>
       </template>
@@ -68,159 +49,71 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref, Ref, onMounted } from 'vue';
-import { DeleteOutlined, EditOutlined, PlusOutlined } from '@ant-design/icons-vue';
-import { notification } from 'ant-design-vue';
-import Post from '@/models/Post'; // Подключение модели поста
-import axios from 'axios'; // Подключение Axios для выполнения HTTP-запросов
+<script setup lang="ts">
+  import { ref, computed, onMounted } from 'vue';
+  import { usePostStore } from '../store/store';
+  import { DeleteOutlined, EditOutlined, PlusOutlined } from '@ant-design/icons-vue';
+  import Post from '@/models/Post';
 
-export default defineComponent({
-  components: {
-    DeleteOutlined, EditOutlined, PlusOutlined // Подключение иконок из Ant Design
-  },
-  name: 'PostList', // Имя компонента
-  setup() {
-    // Инициализация переменных состояния
-    const posts: Ref<Post[]> = ref([]); // Массив постов
-    const editModalVisible = ref(false); // Флаг видимости модального окна редактирования
-    const formData = ref<Post>({ userId: 0, id: 0, title: '', body: '' }); // Данные формы редактирования
-    const addPostModalVisible = ref(false); // Флаг видимости модального окна добавления
-    const newPostTitle = ref(''); // Новый заголовок поста
-    const newPostBody = ref(''); // Новый текст поста
-    const loadingPosts = ref(true); // Флаг загрузки постов
-    const visiblePosts: Ref<Post[]> = ref([]);
-    const loadedPostCount = ref(12);
-    const displayedPostCount = ref(0);
+  // Инициализация хранилища постов и реактивных переменных для модальных окон и данных формы
+  const postStore = usePostStore();
+  const newPostTitle = ref('');
+  const newPostBody = ref('');
+  const addPostModalVisible = ref(false);
+  const editModalVisible = ref(false);
+  const formData = ref<Post>({ userId: 0, id: 0, title: '', body: '' });
+  const loadedPostCount = ref(12);
+  const displayedPostCount = ref(0);
 
-    // Функция для загрузки списка постов
-    const fetchPosts = async () => {
-      try {
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
-        posts.value = response.data;
-        loadInitialPosts();
-      } catch (error) {
-        console.error('Ошибка при загрузке списка постов:', error);
-      } finally {
-        loadingPosts.value = false;
-      }
-    };
+  // Вычисляемые свойства для управления состоянием загрузки и списка постов
+  const loadingPosts = computed(() => postStore.loadingPosts);
+  const posts = computed(() => postStore.posts);
+  const visiblePosts = computed(() => posts.value.slice(0, 12));
 
-    const loadInitialPosts = () => {
-      const endIndex = Math.min(loadedPostCount.value, posts.value.length);
-      visiblePosts.value = posts.value.slice(0, endIndex);
-      displayedPostCount.value = endIndex;
-    };
+  // Функции для управления модальными окнами и операциями с постами
+  const showAddPostModal = () => {
+    addPostModalVisible.value = true;
+  };
 
-    // Функция для удаления поста
-    const deletePost = async (postId: number) => {
-      try {
-        await axios.delete(`https://jsonplaceholder.typicode.com/posts/${postId}`);
-        posts.value = posts.value.filter(post => post.id !== postId);
-        showNotification(postId);
-        console.log('Пост успешно удален');
-      } catch (error) {
-        console.error('Ошибка при удалении поста:', error);
-      }
-    };
+  const addOrSavePost = async () => {
+    await postStore.createPost({ title: newPostTitle.value, body: newPostBody.value, id: 0, userId: 0 });
+    addPostModalVisible.value = false;
+    newPostTitle.value = '';
+    newPostBody.value = '';
+  };
 
-    const showNotification = (postId: Number) => {
-      notification.success({
-        message: 'Post deleted',
-        description: `Post ${postId} was successfully deleted`,
-        duration: 4.5
-      });
-    };
+  const editPost = (post: Post) => {
+    formData.value = { ...post };
+    editModalVisible.value = true;
+  };
 
-    // Функция для открытия модального окна редактирования
-    const editPost = (post: Post) => {
-      formData.value = { ...post };
-      editModalVisible.value = true;
-    };
+  const saveEditedPost = async () => {
+    await postStore.updatePost(formData.value);
+    editModalVisible.value = false;
+  };
 
-    // Функция для сохранения отредактированного поста
-    const saveEditedPost = async () => {
-      try {
-        await axios.put(`https://jsonplaceholder.typicode.com/posts/${formData.value.id}`, formData.value);
-        const index = posts.value.findIndex(post => post.id === formData.value.id);
-        if (index !== -1) {
-          posts.value[index] = { ...formData.value };
-        }
-        console.log('Пост успешно отредактирован');
-        editModalVisible.value = false;
-      } catch (error) {
-        console.error('Ошибка при редактировании поста:', error);
-      }
-    };
+  const deletePost = (postId: number) => {
+    postStore.deletePost(postId);
+  };
 
-    // Функция для добавления и сохранения нового поста
-    const addOrSavePost = async () => {
-      try {
-        const response = await axios.post('https://jsonplaceholder.typicode.com/posts', {
-          title: newPostTitle.value,
-          body: newPostBody.value
-        });
+  // Функция для определения, нужно ли показывать кнопку "Загрузить больше"
+  const shouldShowLoadMoreButton = () => {
+    return displayedPostCount.value < posts.value.length;
+  };
 
-        const newPostData = response.data;
-        const newPostObject = {
-          id: newPostData.id,
-          userId: newPostData.userId,
-          title: newPostData.title,
-          body: newPostData.body
-        };
+  // Функция для загрузки дополнительных постов
+  const loadMorePosts = () => {
+    const startIndex = displayedPostCount.value;
+    const endIndex = Math.min(startIndex + loadedPostCount.value, posts.value.length);
+    for (let i = startIndex; i < endIndex; i++) {
+      visiblePosts.value.push(posts.value[i]);
+    }
+    displayedPostCount.value = endIndex;
+  };
 
-        posts.value.push(newPostObject);
-        console.log('New post successfully added:', newPostObject);
-        addPostModalVisible.value = false;
-        newPostTitle.value = '';
-        newPostBody.value = '';
-      } catch (error) {
-        console.error('Error adding new post:', error);
-      }
-    };
-
-    // Функция для отображения модального окна добавления нового поста
-    const showAddPostModal = () => {
-      addPostModalVisible.value = true;
-    };
-
-    const loadMorePosts = () => {
-      const startIndex = displayedPostCount.value;
-      const endIndex = Math.min(startIndex + loadedPostCount.value, posts.value.length);
-      for (let i = startIndex; i < endIndex; i++) {
-        visiblePosts.value.push(posts.value[i]);
-      }
-      displayedPostCount.value = endIndex;
-    };
-
-    const shouldShowLoadMoreButton = () => {
-      return displayedPostCount.value < posts.value.length;
-    };
-
-    // Загрузка списка постов при монтировании компонента
-    onMounted(fetchPosts);
-
-    // Возврат переменных и функций для использования в шаблоне
-    return {
-      posts,
-      editModalVisible,
-      formData,
-      deletePost,
-      editPost,
-      saveEditedPost,
-      addOrSavePost,
-      showAddPostModal,
-      addPostModalVisible,
-      newPostTitle,
-      newPostBody,
-      loadingPosts,
-      showNotification,
-      loadMorePosts,
-      visiblePosts,
-      shouldShowLoadMoreButton
-    };
-  }
-});
+  onMounted(() => {
+    postStore.fetchPosts();
+  });
 </script>
 
 <style scooped>
---
+++ src/store/store.ts
@@ -0,0 +1,54 @@
+import { defineStore } from 'pinia';
+import axios from 'axios';
+import Post from '@/models/Post';
+
+// Определение хранилища usePostStore
+export const usePostStore = defineStore({
+  id: 'post',
+  state: () => ({
+    posts: [] as Post[],
+    loadingPosts: true,
+  }),
+  actions: {
+    async fetchPosts() {
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+        this.posts = response.data;
+      } catch (error) {
+        console.error('Ошибка при загрузке списка постов:', error);
+      } finally {
+        this.loadingPosts = false;
+      }
+    },
+    async createPost(newPost: Post) {
+      try {
+        const response = await axios.post('https://jsonplaceholder.typicode.com/posts', newPost);
+        this.posts.unshift(response.data);
+        console.log('Пост успешно создан:', response.data);
+      } catch (error) {
+        console.error('Ошибка при создании поста:', error);
+      }
+    },
+    async updatePost(updatedPost: Post) {
+      try {
+        await axios.put(`https://jsonplaceholder.typicode.com/posts/${updatedPost.id}`, updatedPost);
+        const index = this.posts.findIndex(post => post.id === updatedPost.id);
+        if (index !== -1) {
+          this.posts[index] = updatedPost;
+        }
+        console.log('Пост успешно обновлен:', updatedPost);
+      } catch (error) {
+        console.error('Ошибка при обновлении поста:', error);
+      }
+    },
+    async deletePost(postId: number) {
+      try {
+        await axios.delete(`https://jsonplaceholder.typicode.com/posts/${postId}`);
+        this.posts = this.posts.filter(post => post.id !== postId);
+        console.log('Пост успешно удален');
+      } catch (error) {
+        console.error('Ошибка при удалении поста:', error);
+      }
+    },
+  },
+});
